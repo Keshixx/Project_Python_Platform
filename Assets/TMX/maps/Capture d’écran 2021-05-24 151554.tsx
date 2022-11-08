0afcbb0d95526333580b1a21dbd6e64e95421ec4
Keshixx <93916736+Keshixx@users.noreply.github.com>
2022-11-08 16:27:32 +0100

    Add files Assets TMX for map
    Add TMX with the map of game

---
+++ Assets/TMX/maps/Capture d’écran 2021-05-24 151554.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Capture d’écran 2021-05-24 151554" tilewidth="16" tileheight="16" tilecount="630" columns="30">
+ <image source="Capture d’écran 2021-05-24 151554.png" trans="000000" width="492" height="341"/>
+</tileset>
